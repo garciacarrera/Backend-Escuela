337781218d34d239813b2c7c628484c013e4f302
NotMuuns <santicipo21@gmail.com>
2025-06-16 20:07:33 -0300

    cree una nueva rama alumno

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import { envs } from './src/config/envs.js';
 import { profesorController } from './src/controllers/profesorControllers.js';
+import { alumnoRouter } from './src/routers/alumnos.js';
 
 
 const app = express();
 
 
 
-
+app.use(express.json());
 app.use(express.static('./src/public'));
+app.use("/alumno", alumnoRouter);
 
 app.set('port', envs.PORT);
 
 app.get('/profesor', profesorController.getprofesor)
+
 export default app;
+
---
+++ src/routers/alumnos.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { alumnoController } from "../controllers/alumnoControllers.js";
+const alumnoRouter = Router();
+
+alumnoRouter.post("/:alumnoId/tarea", alumnoController.subirTarea);
+
+export default alumnoRouter;
