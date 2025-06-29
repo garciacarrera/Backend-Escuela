e4f0a19dc2361c5e92d3e0b9b54c62e1ad073c73
Golver2909 <matiasfuentes2909@gmail.com>
2025-06-29 18:32:48 -0300

    Rutas de matricula creadas

---
+++ src/routers/matriculas.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { matriculaController } from "../controllers/matriculaControllers";
+
+const matriculaRoutes = Router()
+
+matriculaRoutes.get('/matricula',matriculaController.getMatricula)
+matriculaRoutes.post('/matricula',matriculaController.createMatricula)
+matriculaRoutes.patch('/matricula')
+matriculaRoutes.delete('/matricula/:id')
